72b36c88bffd03d23e0a7ab059f82de82abe7506
Joern <Joern@Joern-PC.fritz.box>
2016-04-05 19:35:36 +0200

    multiple workers can work in a building. STIER

--- src/Buildings/ProducingBuilding.java
+++ src/Buildings/ProducingBuilding.java
@@ -8,6 +8,9 @@ import Game.Storage;
 
 public abstract class ProducingBuilding extends Building implements Producing{
 	static Timer timer = new Timer();
+	private int workers = 0; 
+	private int activeWorkers = 0;
+	private int maxWorkers = 1;
 	
 	private boolean isWorking = false;
 	RessourceType type;
@@ -19,14 +22,18 @@ public abstract class ProducingBuilding extends Building implements Producing{
 	}
 	private void work(){
 		if(isWorking){
-			TimerTask task = new TimerTask() {
-				@Override
-				public void run() {
-					produce();
-					work();
-				}
-			};
-			timer.schedule(task, getProductionTime());
+			while(activeWorkers < workers){
+				TimerTask task = new TimerTask() {
+					@Override
+					public void run() {
+						produce();
+						activeWorkers--;
+						work();
+					}
+				};
+				timer.schedule(task, getProductionTime());
+				activeWorkers++;
+			}
 		}
 	}
 	private int getProductionTime(){
@@ -40,5 +47,24 @@ public abstract class ProducingBuilding extends Building implements Producing{
 	}
 	public void stopWork(){
 		isWorking = false;
+	}
+	public void setMaxWorkers(int maxWorkers) {
+		this.maxWorkers = maxWorkers;
+	}
+	public int getWorkerCount() {
+		return workers;
+	}
+	public void addWorker() {
+		if(!(getWorkerCount()>= maxWorkers)){
+			this.workers++;
+		}
+		if(isWorking){
+			work();
+		}
+	}
+	public void removeWorker(){
+		if(getWorkerCount()>0){
+			this.workers--;
+		}
 	}
 }
--- src/Test/tProducingBuilding.java
+++ src/Test/tProducingBuilding.java
@@ -15,30 +15,94 @@ public class tProducingBuilding {
 	@Test
 	public void test() {
 		int productionTime = 100;
-		
 		class TestBuilding extends ProducingBuilding {
 			int counter = 0;
 			public TestBuilding() {
 				super(0,0,0,0,null,productionTime,null);
 			}
-
+			
 			@Override
 			public void produce() {
 				counter ++;
 			}
+			public void resetCounter(){
+				counter = 0;
+			}
 			
 		}
-
 		TestBuilding building = new TestBuilding();
+		//adding/removing workers
+		assertEquals(0, building.getWorkerCount());
+		building.setMaxWorkers(2);
+		building.addWorker();
+		assertEquals(1, building.getWorkerCount());
+		building.addWorker();
+		assertEquals(2, building.getWorkerCount());
+		building.addWorker();
+		assertEquals(2, building.getWorkerCount());
+		building.removeWorker();
+		assertEquals(1, building.getWorkerCount());
+		building.removeWorker();
+		assertEquals(0, building.getWorkerCount());
+		building.removeWorker();
+		assertEquals(0, building.getWorkerCount());
+
+		
+		// test working with 0 workers
 		building.startWork();
 		try {
-			Thread.sleep(10*productionTime + 10);
+			Thread.sleep(10*productionTime - 20);
 			building.stopWork();
+			Thread.sleep(50);
+			assertEquals(0, building.counter); //produce called 5 times
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// test working with 1 workers
+		building.addWorker();
+		building.startWork();
+		try {
+			Thread.sleep(10*productionTime - 20);
+			building.stopWork();
+			Thread.sleep(50);
 			assertEquals(10, building.counter); //produce called 5 times
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		// test working with 2 workers
+		building.resetCounter();
+		building.addWorker();
+		building.startWork();
+		try {
+			Thread.sleep(10*productionTime - 20);
+			building.stopWork();
+			Thread.sleep(50);
+			assertEquals(20, building.counter); //produce called 5 times
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//test starting work with adding worker
+		building.removeWorker();
+		building.removeWorker();
+		assertEquals(0, building.getWorkerCount());
+		building.resetCounter();
+		building.startWork();
+		try {
+			Thread.sleep(10*productionTime + 10);
+			building.addWorker();
+			Thread.sleep(10*productionTime - 20);
+			building.stopWork();
+			Thread.sleep(50);
+			assertEquals(10, building.counter); //produce called 5 times
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 	}
 
